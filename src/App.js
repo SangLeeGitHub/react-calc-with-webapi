6f3f62e9a5e258edf3ddeed566458dd9a372d7c3
slime <slime@daum.net>
2020-05-04 20:44:54 -0400

    Add the fetch part

--- src/App.js
+++ src/App.js
@@ -4,16 +4,50 @@ import "./App.css";
 
 class App extends React.Component {
   state = {
-    input: ""
+      input: "",
   };
+  operator = "";
+  command = "";
 
   addToInput = num => {
-    console.log({ input: this.state.input + num });
-    this.setState({ input: this.state.input + num });
-  };
 
-  calculateResult = () => {
-    this.setState({ input: eval(this.state.input) });
+      switch (num) {
+          case "+":
+              this.operator = "+"; this.command = "plus";
+              break;
+          case "-":
+              this.operator = "-"; this.command = "minus";
+              break;
+          case "*":
+              this.operator = "*"; this.command = "multiply";
+              break;
+          case "/":
+              this.operator = "/"; this.command = "divide";
+              break;
+          case "^":
+              this.operator = "^"; this.command = "power";
+              break;
+          case "%":
+              this.operator = "%"; this.command = "modulo";
+              break;
+          case "C":
+              this.setState({ input: "" });
+              this.command = "";
+              this.operator = "";
+              return;
+          case "=":
+              const number = this.state.input.split(this.operator);
+              fetch('http://localhost:8080/' + this.command + '/' + number[0] + '/' + number[1])
+                  .then(res => res.json())
+                  .then((data) => {
+                      this.setState({ input: data })
+                  })
+                  .catch(console.log)
+              break;
+          default:
+      }
+
+      this.setState({ input: this.state.input + num });
   };
 
   createRow = arr =>
@@ -27,12 +61,19 @@ class App extends React.Component {
           <div>
             <span>React Calculator</span>
           </div>
-
-          <input className="display" value={this.state.input} />
-          <div className="buttonWrap">{this.createRow([9, 8, 7, "*"])}</div>
-          <div className="buttonWrap">{this.createRow([6, 5, 4, "+"])}</div>
-          <div className="buttonWrap">{this.createRow([3, 2, 1, "-"])}</div>
-          <div className="buttonWrap">{this.createRow([0, ".", "=", "/"])}</div>
+            <input className="display" value={this.state.input} />
+            <div className="buttonWrap">{this.createRow([9, 8, 7, "*"])}</div>
+            <div className="buttonWrap">{this.createRow([6, 5, 4, "+"])}</div>
+            <div className="buttonWrap">{this.createRow([3, 2, 1, "-"])}</div>
+            <div className="buttonWrap">{this.createRow([0, "%", "^", "/"])}</div>
+            <div className="buttonWrap">
+                <div className="doubleBtn">
+                    <NumberButton addToInput={this.addToInput} num="C" />
+                </div>
+                <div className="doubleBtn">
+                    <NumberButton addToInput={this.addToInput} num="=" />
+                </div>
+          </div>
         </div>
     );
   }
